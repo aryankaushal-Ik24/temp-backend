eb7bbf0c1dfa1efe5563136c57eb8b3a070859c1
unknown <aryan kaushal>
2025-05-01 20:54:08 +0530

    changes made v3

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const API_KEY = process.env.API_KEY;
-const API_SECRET = process.env.API_SECRET;
+const API_KEY = 'd58d81c5f8fa4c477cda2118c4ea0c2d';
+const API_SECRET = '84dba585c5561f7c46b348c0598c054a';
 
 app.get('/auth/callback', async (req, res) => {
   const { code, shop } = req.query;
 
   try {
+    console.log("code",code);
+    console.log("shop",shop);
     const response = await axios.post(`https://${shop}/admin/oauth/access_token`, {
       client_id: API_KEY,
       client_secret: API_SECRET,
