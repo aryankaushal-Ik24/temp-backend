2c0b3a4bf0292a23eb30f8d67c806114299e89d6
unknown <aryan kaushal>
2025-05-08 15:53:51 +0530

    backend with delta v1.8

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { addSelectedSceneWithProduct, getProductInformation } = require('./contro
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3001','http://localhost:3000'], 
+  origin: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true 
