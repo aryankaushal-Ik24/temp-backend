fa97bc2dd455577bd73152b78cb1d433f7e22502
aryankaushal-Ik24 <aryan.kaushal@ikarusdelta.com>
2025-09-09 18:14:21 +0530

    feat: added eslint and .github file

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,    // Node.js globals (process, __dirname, Buffer, etc.)
+        ...globals.es2021,  // Modern JS globals (Promise, Map, etc.)
+      },
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+  },
+]);
